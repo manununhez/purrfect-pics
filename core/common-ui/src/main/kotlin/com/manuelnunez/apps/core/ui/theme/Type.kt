7be96865eaee0653350d037a3049fc469ca25022
Manuel Nuñez <03.manu@gmail.com>
2024-04-11 23:29:37 -0400

    Updated theme fonts #16

--- core/common-ui/src/main/kotlin/com/manuelnunez/apps/core/ui/theme/Type.kt
+++ core/common-ui/src/main/kotlin/com/manuelnunez/apps/core/ui/theme/Type.kt
@@ -12,33 +12,33 @@ val Typography =
     Typography(
         displaySmall =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans_light)),
                 fontWeight = FontWeight.Normal,
                 fontSize = 36.sp),
         headlineSmall =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans_light)),
                 fontWeight = FontWeight.Normal,
                 fontSize = 30.sp),
         labelSmall =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans_light)),
                 fontWeight = FontWeight.Normal,
                 fontSize = 13.sp),
         titleSmall =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2_bold)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans_bold)),
                 fontWeight = FontWeight.Bold,
                 fontSize = 14.sp),
         titleMedium =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2_medium)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans)),
                 fontWeight = FontWeight.SemiBold,
                 letterSpacing = (.5).sp,
                 fontSize = 18.sp),
         titleLarge =
             TextStyle(
-                fontFamily = FontFamily(Font(R.font.baloo_paaji_2)),
+                fontFamily = FontFamily(Font(R.font.nunito_sans_light)),
                 fontWeight = FontWeight.Normal,
                 fontSize = 24.sp),
     )
