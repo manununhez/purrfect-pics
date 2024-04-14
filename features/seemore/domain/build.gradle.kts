cb32b7b23d3ded34b81b89121fc010598155970a
Manuel Nuñez <03.manu@gmail.com>
2024-04-14 08:42:42 -0400

    Added seemore screen config #27

---
+++ features/seemore/domain/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.ksp)
+}
+
+android {
+  namespace = "com.manuelnunez.apps.features.seemore.domain"
+  compileSdk = 34
+
+  defaultConfig { minSdk = 21 }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+
+  tasks.withType<Test> { useJUnitPlatform() }
+
+  packaging { resources { excludes.add("META-INF/{LICENSE-notice.md,LICENSE.md}") } }
+}
+
+dependencies {
+  implementation(projects.core.common)
+  implementation(projects.core.domain)
+
+  implementation(libs.kotlinx.coroutines.android)
+
+  // HILT
+  implementation(libs.hilt.android)
+  ksp(libs.hilt.compiler)
+
+  testImplementation(libs.junit)
+  testImplementation(libs.mockk)
+  testImplementation(libs.turbine)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,3 +41,7 @@ include(":features:home:domain")
 include(":features:home:ui")
 
 include(":features:detail:ui")
+
+include(":features:seemore:ui")
+
+include(":features:seemore:domain")
