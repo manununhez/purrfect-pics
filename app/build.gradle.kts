a157ff7a6afcc7a6082873711408289017ada071
Manuel Nuñez <03.manu@gmail.com>
2024-04-16 14:26:40 -0400

    Update version for release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "com.manuelnunez.apps.purrfectpics"
     minSdk = 21
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.0.1"
   }
 
   signingConfigs {
